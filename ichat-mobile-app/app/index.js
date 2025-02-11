ed4416a529e904e026b944f2fc427de35f9c62e5
unknown <21139431.anh@student.iuh.edu.vn>
2025-02-11 14:52:19 +0700

    Push new code structure

--- ichat-mobile-app/app/index.js
+++ ichat-mobile-app/app/index.js
@@ -1,34 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function Page() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+import LauncherScreen from "../src/screens/LauncherScreen";
+
+export default function App() {
+  return <LauncherScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
---
+++ ichat-mobile-app/src/screens/LauncherScreen.js
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  StatusBar,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+const LauncherScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/images/background.png")}
+        style={styles.background}
+      >
+        <View style={styles.content}>
+          <Text style={styles.title}>iChat</Text>
+          <View>
+            <Image
+              source={require("../assets/images/logo.png")}
+              style={{ height: 500, width: 500 }}
+            />
+          </View>
+          <View>
+            <TouchableOpacity></TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default LauncherScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  content: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 70,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
