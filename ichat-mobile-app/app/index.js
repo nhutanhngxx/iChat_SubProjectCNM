70b3f2b841f900407ec01dbafce957ce56bf3897
ngthcuong <nguyenthanh8a2@gmail.com>
2025-02-11 15:53:48 +0700

    mobile : setup navigation buttom tab

--- ichat-mobile-app/app/index.js
+++ ichat-mobile-app/app/index.js
@@ -1,10 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-const Stack = createNativeStackNavigator();
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import LauncherScreen from "../src/screens/LauncherScreen";
+import HomeScreen from "../src/screens/HomeScreen";
+import LoginScreen from "../src/screens/LoginScreen";
 
 export default function App() {
-  return <LauncherScreen />;
+  const Stack = createNativeStackNavigator();
+  const ButtomTab = createBottomTabNavigator();
+  return (
+    <ButtomTab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#00bdd6",
+        // tabBarShowLabel: false,
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+      }}
+    >
+      <ButtomTab.Screen name="Launcher" component={LauncherScreen} />
+      <ButtomTab.Screen name="Login" component={LoginScreen} />
+      <ButtomTab.Screen name="Home" component={HomeScreen} />
+    </ButtomTab.Navigator>
+  );
 }
