470c03f98c2a112284d646948a1d5112314a1cf0
ngthcuong <nguyenthanh8a2@gmail.com>
2025-02-26 01:47:35 +0700

    add: FriendTab.js, GroupTab.js in ContactTab

---
+++ ichat-mobile-app/src/components/header/HeaderContactTab.js
@@ -0,0 +1,39 @@
+import { View, Image, TextInput } from "react-native";
+
+const HeaderContactTab = () => {
+  return (
+    <View
+      style={{
+        height: 40,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 10,
+      }}
+    >
+      <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
+        <Image
+          source={require("../../assets/icons/search.png")}
+          style={{ width: 20, height: 20 }}
+        />
+        <TextInput
+          style={{
+            fontSize: 18,
+            width: "100",
+            color: "#2F80ED",
+          }}
+          placeholder="Tìm kiếm"
+        ></TextInput>
+      </View>
+
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Image
+          source={require("../../assets/icons/add-friend.png")}
+          style={{ width: 20, height: 20 }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default HeaderContactTab;
--- ichat-mobile-app/src/screens/ContactTab.js
+++ ichat-mobile-app/src/screens/ContactTab.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Button, SafeAreaView, StyleSheet } from "react-native";
+import { Tab } from "@rneui/themed";
+
+import HeaderContactTab from "../components/header/HeaderContactTab";
+import FriendTab from "../components/contact/FriendTab";
+import GroupTab from "../components/contact/GroupTab";
+import { TabView } from "@rneui/base";
 
 const ContactTab = () => {
+  const [index, setIndex] = useState(0);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Contact Screen</Text>
+      <HeaderContactTab />
+      <Tab
+        value={index}
+        onChange={setIndex}
+        indicatorStyle={{
+          backgroundColor: "skyblue",
+          width: "25%",
+          marginHorizontal: "12.5%",
+          marginBottom: 5,
+        }}
+        variant="default"
+        dense
+      >
+        <Tab.Item
+          title={"Friend"}
+          titleStyle={{ color: "black", fontWeight: "bold" }}
+        ></Tab.Item>
+        <Tab.Item
+          title={"Group"}
+          titleStyle={{ color: "black", fontWeight: "bold" }}
+        />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex} animationType="spring">
+        <TabView.Item>
+          <FriendTab />
+        </TabView.Item>
+        <TabView.Item>
+          <GroupTab />
+        </TabView.Item>
+      </TabView>
     </SafeAreaView>
   );
 };
