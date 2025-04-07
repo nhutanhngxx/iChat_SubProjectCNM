a5dee55ed43c63d6b9a398ff4731bbab8797644e
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2025-04-07 20:05:51 +0700

    Theme Color

--- ichat-web-app/src/App.js
+++ ichat-web-app/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/global.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Login from "./components/Login";
 import HomeWeb from "./components/HomeWeb";
 import Test from "./components/HomeWeb/ChatWindow/test";
-import { ThemeProvider } from "./components/HomeWeb/DropDownList/SettingsModal/TabSetting/ThemeContext";
 import {PersistGate} from "redux-persist/integration/react";
 import {persistor} from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <ThemeProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
@@ -21,7 +20,6 @@ function App() {
             <Route path="/test" element={<Test />} />
           </Routes>
         </BrowserRouter>
-      </ThemeProvider>
       </PersistGate>
     </Provider>
   );
