e2a57b0b5ede388bfeb3def8c4e5e64fc4b83120
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-04-17 00:54:38 +0700

    Feat: Enhance user experience in various components with improved UI and functionality

--- ichat-mobile-app/components/header/HeaderOption.js
+++ ichat-mobile-app/components/header/HeaderOption.js
@@ -1,27 +1,50 @@
 import React from "react";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const HeaderOption = () => {
   const navigation = useNavigation();
   return (
     <View
-      style={{
-        flexDirection: "row",
-        alignItems: "flex-end",
-        height: 90,
-        padding: 10,
-      }}
+      style={
+        Platform.OS === "ios"
+          ? {
+              flexDirection: "row",
+              alignItems: "flex-end",
+              width: "100%",
+              padding: 10,
+              height: 90,
+              backgroundColor: "#007bff",
+            }
+          : {
+              flexDirection: "row",
+              alignItems: "flex-end",
+              width: "100%",
+              paddingHorizontal: 10,
+              height: 80,
+              backgroundColor: "#007bff",
+            }
+      }
     >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 5,
+        }}
+      >
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Image
             source={require("../../assets/icons/go-back.png")}
-            style={{ width: 25, height: 25 }}
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: "white",
+            }}
           />
         </TouchableOpacity>
-        <Text style={{ fontSize: 20, fontWeight: "bold", marginLeft: 10 }}>
-          Tùy chọn
+        <Text style={{ fontWeight: "bold", fontSize: 20, color: "white" }}>
+          Tùy chọn cuộc trò chuyện
         </Text>
       </View>
     </View>
