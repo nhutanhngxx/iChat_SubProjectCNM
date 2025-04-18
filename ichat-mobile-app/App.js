73af38a9acfb49bf741260c440c43378adbb9e7b
nhutanhngxx <21139431.anh@student.iuh.edu.vn>
2025-04-18 17:16:26 +0700

    Refactor: Remove SocketProvider and related context from App component

--- ichat-mobile-app/App.js
+++ ichat-mobile-app/App.js
@@ -1,16 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./navigation/AppNavigator";
 import { UserProvider } from "./config/context/UserContext";
-import { SocketProvider } from "./config/context/SocketContext";
 
 export default function App() {
   return (
     <UserProvider>
-      <SocketProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </SocketProvider>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
     </UserProvider>
   );
 }
