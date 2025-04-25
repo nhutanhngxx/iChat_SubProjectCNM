b4b0cf8a4a28897bdad4ffdfe05853b954502a17
ngthcuong <nguyenthanh8a2@gmail.com>
2025-04-25 22:18:30 +0700

    chore: update display in ModalMemberManagement

--- ichat-mobile-app/components/group/ModalMemberManagement.js
+++ ichat-mobile-app/components/group/ModalMemberManagement.js
@@ -99,20 +99,7 @@ const ModalMemberManagement = ({ route }) => {
     // Lắng nghe sự kiện thành viên được chấp nhận vào nhóm
     socketService.onMemberAccepted(({ groupId: receivedGroupId, memberId }) => {
       if (groupId === receivedGroupId) {
-        setMembers((prev) =>
-          prev.map((member) =>
-            member.user_id === memberId
-              ? { ...member, status: "approved" }
-              : member
-          )
-        );
-        setFilteredMembers((prev) =>
-          prev.map((member) =>
-            member.user_id === memberId
-              ? { ...member, status: "approved" }
-              : member
-          )
-        );
+        fetchGroupMembers();
       }
     });
 
