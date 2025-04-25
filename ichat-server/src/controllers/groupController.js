a036da022bc0ba8c0288f3f6c14201df9968ceb7
ngthcuong <nguyenthanh8a2@gmail.com>
2025-04-26 00:16:56 +0700

    fix: update avatar handling in group creation to support file object directly

--- ichat-server/src/controllers/groupController.js
+++ ichat-server/src/controllers/groupController.js
@@ -171,7 +171,7 @@ const GroupController = {
     try {
       const { groupId } = req.params;
       const { name } = req.body;
-      const avatar = req.file ? req.file.buffer : null;
+      const avatar = req.file || null;
       const allow_add_members = req.body.allow_add_members || true;
       const allow_change_name = req.body.allow_change_name || true;
       const allow_change_avatar = req.body.allow_change_avatar || true;
